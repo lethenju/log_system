fa5fe81067932f43fd49c2847169fc4a3d678ffa
Julien LE THENO <julien.letheno@gmail.com>
2019-02-04 17:20:28 +0100

    clarify main

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 /*
- * This file is an example to the log system behaviour. It shows how to initiates it
- * 
+ * This file is an example to the log system behaviour. It shows how to initiates it, 
+ * and briefly how to use it
  */ 
-
 #include "log_system.h"
-#include "stdio.h"
 
 int main(int argc, char* argv[]) {
     log_init();
     pthread_create(&log_pthread, NULL, (void*) log_thread, (void*) NULL);
     
-    log_add("test");
+    log_add("test"); // Not working for now
     
     
     pthread_join(log_pthread,NULL);
