bc35ee89e98295df6006b372ff7cf2332aa48637
Julien LE THENO <julien.letheno@gmail.com>
2019-02-06 16:16:34 +0100

    variable number of args

--- log_system.c
+++ log_system.c
@@ -1,6 +1,7 @@
 #include "log_system.h"
 #include "stdio.h"
 #include <stdlib.h>
+#include <stdarg.h>
 
 struct log *stack_log;
 int nb_logs_in_stack = 0;
@@ -19,23 +20,34 @@ void *log_thread(void)
     int i;
     while (1)
     {
-        log_handle(stack_log, stdout);
-        for (i = 0; i < nb_logs_in_stack; i++)
-        {
-        //   *(stack_log + i) = *(stack_log + i + 1); TODO Fix later
+        if (nb_logs_in_stack > 0){
+            log_handle(stack_log, stdout);
+            nb_logs_in_stack--;
+            for (i = 1; i < nb_logs_in_stack+1; i++)
+            {
+                *(stack_log + i - 1 ) = *(stack_log + i); 
+            }
         }
     }
 }
 /** Handles a log by writing it on output
  */
-int log_add(char* message)
+int log_add(int number_args, int level, const char* file, const char* func, const int line, const char* message, ...)
 {
+   va_list valist;
+
+   /* initialize valist for num number of arguments */
+   va_start(valist, number_args);
+    switch (number_args) {
+        case 2:
+            sprintf(message, message, va_arg(valist, int)); 
+    }
     struct log l = {
-        DEBUG,
+    level,
        time(NULL),
-       "main.c",
-       "lol",
-       "test",
+       func,
+       file,
+       line,
        message
     };
     if (nb_logs_in_stack >= MAX_SIZE_STACK)
@@ -51,16 +63,16 @@ void log_handle(struct log *l, struct _IO_FILE *output)
     switch (l->level)
     {
     case ERROR: // TODO Add color
-        fprintf(output, "[%f][%s][%s][%s] %s\n", (double)time(&l->time), l->mod, l->file, l->func, l->data);
+        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
         break;
     case WARNING:
-        fprintf(output, "[%f][%s][%s][%s] %s\n", (double)time(&l->time), l->mod, l->file, l->func, l->data);
+        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
         break;
     case INFO:
-        fprintf(output, "[%f][%s][%s][%s] %s\n", (double)time(&l->time), l->mod, l->file, l->func, l->data);
+        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
         break;
     case DEBUG:
-        fprintf(output, "[%f][%s][%s][%s] %s\n", (double)time(&l->time), l->mod, l->file, l->func, l->data);
+        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
         break;
     default:
         break;
