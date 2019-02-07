ce14bd5bf1d6dbded886e99c65d4bc9f455a0681
Julien LE THENO <julien.letheno@gmail.com>
2019-02-07 17:25:28 +0100

    working prototype

--- log_system.c
+++ log_system.c
@@ -3,15 +3,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-struct log *stack_log;
-int nb_logs_in_stack = 0;
+struct log_ctx *context;
 
 /** Initialize the log system
  */
 void log_init()
 {
-    stack_log = (struct log *)malloc(MAX_SIZE_STACK * sizeof(struct log));
-    nb_logs_in_stack = 0;
+    context = (struct log_ctx *)malloc(sizeof(struct log_ctx));
+    context->begin = clock();
+    context->stack_log = (struct log *)malloc(MAX_SIZE_STACK * sizeof(struct log));
+    context->nb_logs_in_stack = 0;
+    pthread_create(&log_pthread, NULL, (void*) log_thread, (void*) NULL);
 }
 /** Main loop of the log system.
  */
@@ -20,40 +22,39 @@ void *log_thread(void)
     int i;
     while (1)
     {
-        if (nb_logs_in_stack > 0){
-            log_handle(stack_log, stdout);
-            nb_logs_in_stack--;
-            for (i = 1; i < nb_logs_in_stack+1; i++)
+        if (context->nb_logs_in_stack > 0){
+            log_handle(context->stack_log, stdout);
+            context->nb_logs_in_stack--;
+            for (i = 1; i < context->nb_logs_in_stack+1; i++)
             {
-                *(stack_log + i - 1 ) = *(stack_log + i); 
+                *(context->stack_log + i - 1 ) = *(context->stack_log + i); 
             }
         }
     }
 }
 /** Handles a log by writing it on output
  */
-int log_add(int number_args, int level, const char* file, const char* func, const int line, const char* message, ...)
+int log_add(int level, char* format, ...)
 {
-   va_list valist;
+    va_list valist;
 
-   /* initialize valist for num number of arguments */
-   va_start(valist, number_args);
-    switch (number_args) {
-        case 2:
-            sprintf(message, message, va_arg(valist, int)); 
-    }
+    /* initialize valist for num number of arguments */
+    va_start(valist, format);
+    char* msg = (char*) malloc(1024);
+    vsprintf(msg, format,valist);
+    va_end(valist);
+
     struct log l = {
-    level,
-       time(NULL),
-       func,
-       file,
-       line,
-       message
+       level,
+       (double)(clock()-context->begin)/CLOCKS_PER_SEC,
+       msg
     };
-    if (nb_logs_in_stack >= MAX_SIZE_STACK)
+    if (context->nb_logs_in_stack >= MAX_SIZE_STACK)
         return -1;
-    *(stack_log + nb_logs_in_stack) = l;
-    nb_logs_in_stack++;
+    *(context->stack_log + context->nb_logs_in_stack) = l;
+    context->nb_logs_in_stack++;
+
+   
     return 0;
 }
 /** Handles a log by writing it on output
@@ -63,16 +64,16 @@ void log_handle(struct log *l, struct _IO_FILE *output)
     switch (l->level)
     {
     case ERROR: // TODO Add color
-        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
+        fprintf(output, "\e[31m[%.2f] %s\e[39m\n", (double)l->time, l->data);
         break;
     case WARNING:
-        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
+        fprintf(output, "\e[33m[%.2f] %s\e[39m\n", (double)l->time, l->data);
         break;
     case INFO:
-        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
+        fprintf(output, "\e[34m[%.2f] %s\e[39m\n", (double)l->time, l->data);
         break;
     case DEBUG:
-        fprintf(output, "[%f][%s][%s][%d] %s\n", (double)time(&l->time), l->file, l->func, l->line, l->data);
+        fprintf(output, "\e[32m[%.2f] %s\e[39m\n", (double)l->time, l->data);
         break;
     default:
         break;
