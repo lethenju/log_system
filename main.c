55c491a524cce2961be97caf3a116bce8f5fc0b5
Julien LE THENO <julien.letheno@gmail.com>
2019-02-13 14:44:22 +0100

    test limits of the system with many parallel logs

--- main.c
+++ main.c
@@ -1,21 +1,45 @@
-/*
- * This file is an example to the log system behaviour. It shows how to initiates it, 
- * and briefly how to use it
- */ 
 #include <unistd.h>
 #include "log_system.h"
+pthread_t test, test2, test3;
+void* thread_test(void) {
+    int i;
+    for (i=0; i<100; i++) {
+        usleep(30);
+        log_add(WARNING,"Log number : %d", i);
+    }
+    for (i=0; i<1000; i++) {
+        usleep(30);
+        log_add(WARNING,"Log number : %d", i);
+    }
+}
+void* thread_test2(void) {
+    int i;
+    for (i=0; i<200; i++) {
+        usleep(20);
+        log_add(ERROR,"Log number : %d", i);
+    }
+}
+void* thread_test3(void) {
+    int i;
+    for (i=0; i<100; i++) {
+        usleep(10);
+        log_add(INFO,"Log number : %d", i);
+    }
+}
 
 int main(int argc, char* argv[]) {
     log_init();
-    log_add(DEBUG,"test %s","Debug test");
-    usleep(450000);
-    log_add(INFO,"test %s","Info test");
-    usleep(100000);
-    log_add(ERROR,"Error n %d, critical issue %d", 11, 45);
-    usleep(230000);
-    log_add(WARNING,"WARN%cNG",'I');
+
+    pthread_create(&test, NULL, (void*) thread_test, (void*) NULL);
+    pthread_create(&test2, NULL, (void*) thread_test2, (void*) NULL);
+    pthread_create(&test3, NULL, (void*) thread_test3, (void*) NULL);
+
     
+    pthread_join(test, NULL);
+    pthread_join(test2, NULL);
+    pthread_join(test3, NULL);
     
+
     log_end();
 
     return 0;
